fc00dbba0c9857406ac3ea2c037ab53d0d0aff89
archieran <archan.ranade@gmail.com>
2018-04-19 20:11:46 +0530

    migrations

---
+++ begin.bat
@@ -0,0 +1,3 @@
+@echo off
+cd mraurum
+F:\PG\Sem_2\SEN\Project\JMMS\mr-aurum\flask\Scripts\activate
---
+++ mraurum/mraurum/apps.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class WwwConfig(AppConfig):
+    name = 'www'
+    verbose_name = 'Site'
